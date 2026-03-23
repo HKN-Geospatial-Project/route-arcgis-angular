091ef5a20bb79a278eae70dc1ef60e33aa9a612a
hkazunoda <hkazunoda@gmail.com>
2026-03-23 15:31:16 -0300

    Update library versions

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,18 +2,18 @@
 # .devcontainer/setup.sh
 
 echo "Installing base packages..."
-npm install -g npm@11.10.1
+npm install -g npm@11.12.0
 
-echo "Installing ArcGIS packages to latest..."
+echo "Installing ArcGIS packages..."
 
 echo "- arcgis/core"
-npm install @arcgis/core@5.0.1 
+npm install @arcgis/core@5.0.13
 
 # echo "- arcgis/map-components"
-# npm install @arcgis/map-components@5.0.1 
+# npm install @arcgis/map-components@5.0.13
 
 # echo "- arcgis/charts-components"
-# npm @arcgis/charts-components@5.0.1 
+# npm @arcgis/charts-components@5.0.13
 
 # echo "- esri/calcite-components"
 # npm @esri/calcite-components@5.0.2
