16a798dea56dd31087d79e96641c5dcfb3ba3ce3
hkazunoda <hkazunoda@gmail.com>
2026-03-24 13:42:04 +0000

    Add toastr library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,14 @@ import { MapEventProviderService } from './map-library/abstract/services/map-eve
 import { ArcGISMapEventProviderService } from './map-library/arcgis-map/services/arcgis-map-event-provider.service';
 import { RouteGraphicsService } from './map-library/abstract/services/route-map-graphics.service';
 import { ArcGISRouteGraphicsService } from './map-library/arcgis-map/services/arcgis-route-map-graphics.service';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideAnimations(),
+    provideToastr(),
     provideRouter(routes),
     {
       provide: MapEventProviderService,
